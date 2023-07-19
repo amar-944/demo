3ecc174fd76792f40126b954d3facfe237b6dc46
amar <100714726+amar-944@users.noreply.github.com>
2023-07-19 16:54:01 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -14,3 +14,26 @@ variable "kubernetes_version" {
 
 variable "ssh_key" {
 }
+variable "serviceprinciple_id" {
+}
+
+variable "serviceprinciple_key" {
+}
+
+variable "tenant_id" {
+}
+
+variable "subscription_id" {
+}
+
+
+variable "ssh_key" {
+}
+
+variable "location" {
+  default = "australiaeast"
+}
+
+variable "kubernetes_version" {
+    default = "1.22.2"
+}
