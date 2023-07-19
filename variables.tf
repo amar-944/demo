69ed0b8042592073b8ccbf4151867a79c1a6ec9e
amar <100714726+amar-944@users.noreply.github.com>
2023-07-19 16:52:48 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,16 @@
+variable "serviceprinciple_id" {
+}
+
+variable "serviceprinciple_key" {
+}
+
+variable "location" {
+  default = "eastus"
+}
+
+variable "kubernetes_version" {
+    default = "1.25.6"
+}
+
+variable "ssh_key" {
+}
